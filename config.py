fa995dae6d3c3e4646deb30e2ebb9961b5e39d33
Vince Kim <dkim16giants@gmail.com>
2025-02-22 16:28:18 -0500

    Test MongoDB Cloud Connection
    Mahatma's MongoDB successfully fetched and stored all data from IA, 1129 items

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 MISTRAL_API_KEY = "your_api_key_here"
-MONGO_URI = "mongodb://localhost:27017/"
+# MONGO_URI = "mongodb://localhost:27017/" # Local MongoDB connection
+MONGO_URI = "mongodb+srv://hatmatty:mLT7nUkr6OLdc0xW@cluster0.enmtn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DATABASE_NAME = "cdc_database"
 COLLECTION_NAME = "datasets"
 CONFIG_COLLECTION = "config"
 
 # Automatically fetch datasets on app start
-AUTO_FETCH_ON_START = False  # Implementer should change this if updates are needed
+AUTO_FETCH_ON_START = True  # Implementer should change this if updates are needed
