b25194ad201afef6e57184b7297d28feff8b05c7
Mahatma Kollu <mkollu3@gatech.edu>
2025-02-23 05:44:38 -0500

    remove title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,9 +8,6 @@ const App = () => {
     <Router>
       <div className="max-w-3xl mx-auto p-4">
         {/* Make the h1 a link to the homepage */}
-        <Link to="/" className="block">
-          <h1 className="text-3xl font-bold text-center mb-4">Vesalius Dataset Search</h1>
-        </Link>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/dataset/:datasetId" element={<DatasetPage />} />
